2a2c2f5266605c0e3bb0712be2be54b29dbb5e35
ZhuofanWu <zhuofanwu@foxmail.com>
2024-07-07 21:21:59 +0800

    User password change&delete Mapper

--- src/main/java/com/fancy/internsys/mapper/UserMapper.java
+++ src/main/java/com/fancy/internsys/mapper/UserMapper.java
@@ -1,9 +1,7 @@
 package com.fancy.internsys.mapper;
 
 import com.fancy.internsys.pojo.UserIdentity;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserMapper {
@@ -12,4 +10,10 @@ public interface UserMapper {
 
     @Select("SELECT UUID,login_mail,password,role FROM user_basic WHERE login_mail = #{login_mail}")
     UserIdentity searchUser(String login_mail);
+
+    @Update("UPDATE user_basic SET password=#{password} WHERE login_mail = #{login_mail}")
+    void changePasswordUser(@Param("login_mail") String login_mail,@Param("password") String password);
+
+    @Delete("DELETE from user_basic where login_mail=#{login_mail}")
+    void deleteUser(String login_mail);
 }
